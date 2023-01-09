689f1445038a0fea57df87f2f9bab1b0f4270649
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-01-09 19:27:50 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fileinput = document.querySelector(".file-input"),
+filteroptions=document.querySelectorAll(".filter button");
 previmg=document.querySelector(".preview-img img");
 chooseimgbbutoon=document.querySelector(".choose-img");
 
@@ -8,7 +9,18 @@ const loadImg=()=>{
     console.log(file);
     if(!file) return;
     previmg.src=URL.createObjectURL(file);
+    previmg.addEventListener("load", () =>{
+        document.querySelector(".container").classList.remove("disable");
+    });
 }
+
+filteroptions.forEach(option => {
+    option.addEventListener("click", () =>{
+        console.log(option);
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+    });
+})
 
 fileinput.addEventListener("change",loadImg);
 chooseimgbbutoon.addEventListener("click", () => fileinput.click());
